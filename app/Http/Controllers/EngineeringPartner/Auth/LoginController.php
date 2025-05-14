c798988c62e0025d0041c73f106120e3ebc5a102
Hazem Ayad <hazem1fadil@gmail.com>
2025-05-14 14:59:01 +0300

    edit db seeders/DatabaseSeeder.php and login for guard EngineeringPartner

--- app/Http/Controllers/EngineeringPartner/Auth/LoginController.php
+++ app/Http/Controllers/EngineeringPartner/Auth/LoginController.php
@@ -21,8 +21,7 @@ class LoginController extends Controller
         ]);
 
         $credentials = $request->only('email', 'password');
-        $credentials['status_cd'] = 16;
-
+ 
         if (Auth::guard('engineering')->attempt($credentials)) {
             $request->session()->regenerate();
 
